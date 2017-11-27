05159518d341a9a0421237606171722823f2cd60
Michael Chi <michi@microsoft.com>
2017-11-27 14:29:50 +0800

    update

--- DispatcherBot/Models/DataSource.cs
+++ DispatcherBot/Models/DataSource.cs
@@ -5,14 +5,17 @@ using System.Web;
 
 namespace DispatcherBot.Models
 {
+    /// <summary>
+    /// This is a mock datasource class, you should change below codes to actually retrieve BOT information from your choosen storage
+    /// </summary>
     public class DataSource
     {
 #if true
         public static Dictionary<string, string> RegisteredBots = new Dictionary<string, string>
         {
             { "IT BOT","ITURL" },
-            { "HR BOT","https://hrbotdemo001.azurewebsites.net/api/messages"},
-            { "Main BOT","https://dispatcherbotdemo.azurewebsites.net/api/messages"}
+            { "HR BOT","https://<HRWEB URL>.azurewebsites.net/api/messages"},
+            { "Main BOT","https://<ROOT BOT URL>.azurewebsites.net/api/messages"}
         };
 #else
         public static Dictionary<string, string> RegisteredBots = new Dictionary<string, string>
@@ -24,8 +27,8 @@ namespace DispatcherBot.Models
 #endif
         public static Dictionary<string, KeyValuePair<string, string>> RegisteredBotSecrets = new Dictionary<string, KeyValuePair<string, string>>
         {
-            { "IT BOT", new KeyValuePair<string,string>("","") },
-            { "HR BOT", new KeyValuePair<string,string>("016141c9-bc4d-4524-a299-5d05e878d2cd","gccbiAWLF198!xdVOP94*+~") }
+            { "IT BOT", new KeyValuePair<string,string>("{ROOT BOT ID}","ROOT BOT PASSWORD") },
+            { "HR BOT", new KeyValuePair<string,string>("{ROOT BOT ID}","{ROOT BOT PASSWORD}") }
         };
     }
 }
